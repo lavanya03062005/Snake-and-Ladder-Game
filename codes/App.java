b50d2c81e994b2cc5febfd59b3c0554857283e95
lavanya03062005 <152963648+lavanya03062005@users.noreply.github.com>
2024-03-18 19:20:26 +0530

    Update App.java

--- codes/App.java
+++ codes/App.java
@@ -10,9 +10,9 @@ public class App {
     public static void main(String[] args) throws Exception {
       HashMap<String,Jumper> map =new HashMap<>();
       map.put("6",new Jumper(new Coordinates(9,5),new Coordinates(6,6)));
-      map.put("6",new Jumper(new Coordinates(0,1),new Coordinates(9,9)));
-      map.put("6",new Jumper(new Coordinates(7,0),new Coordinates(0,9)));
-      map.put("6",new Jumper(new Coordinates(3,3),new Coordinates(7,4)));
+      map.put("97",new Jumper(new Coordinates(0,1),new Coordinates(9,9)));
+      map.put("21",new Jumper(new Coordinates(7,0),new Coordinates(0,9)));
+      map.put("64",new Jumper(new Coordinates(3,3),new Coordinates(7,4)));
        
         System.out.println("Game Snake and Ladder -->");
        Player p1=new Player();
